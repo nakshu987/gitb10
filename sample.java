c883a030b76efd9d38ee34b904dfc387f4dd21ac
venkat <venkat@gmail.com>
2021-09-03 08:49:45 +0530

    modified sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 class Sample{  
 static public void main(String... args){  
-System.out.println("hello world");  
+System.out.println("hello world");
+System.out.println("welcome to the devops traing")
 }  
 };
